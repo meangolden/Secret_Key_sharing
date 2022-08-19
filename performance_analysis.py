0fa2bce113b0553a166afbf8ea7ab13c2bc7ac1b
gugi200 <78741702+gugi200@users.noreply.github.com>
2022-08-19 13:33:22 +0100

    added markers, dotted line, universal font size setting

--- performance_analysis.py
+++ performance_analysis.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Aug 18 21:16:46 2022
-
 @author: cp17593
 """
 
@@ -15,6 +14,7 @@ class KeyStat():
         '''n and tau have to be local'''
         self.alpha=alpha
         self.m = m
+        self.fontsize=11
 
 
     def get_prob_same_keybits(self, n,  tau):
@@ -84,13 +84,14 @@ class KeyStat():
                 pdfMatch= self.prob_matching_keys(n, tau)
                 probMatch.append(pdfMatch)
                      
-            plt.plot(n_list, probMatch, label=f'tau= {tau}')
+            plt.plot(n_list, probMatch, '--o', label=f'tau= {tau}')
             #plt.plot(n_list, probMismatch, label='key mismatch prob')
-        plt.xlabel('Block size length')
-        plt.ylabel('Probability of two matching keys')
-        plt.title(f'alpha= {self.alpha}, m= {self.m}')
+        plt.xticks(n_list)
+        plt.xlabel('Block size length', fontsize=self.fontsize)
+        plt.ylabel('Probability of two matching keys', fontsize=self.fontsize)
+        plt.title(f'alpha= {self.alpha}, m= {self.m}', fontsize=self.fontsize)
         plt.grid()
-        plt.legend()
+        plt.legend(fontsize=self.fontsize)
         
         
         
@@ -104,13 +105,14 @@ class KeyStat():
                 y= self.exp_missmatches(n, tau)
                 ys.append(y)
                  
-            plt.plot(n_list, ys, label=f'tau= {tau}')
+            plt.plot(n_list, ys, '--o', label=f'tau= {tau}')
         #plt.plot(n_list, probMismatch, label='key mismatch prob')
-        plt.xlabel('Block size length')
-        plt.ylabel('Expected Number of Missmatches')
-        plt.title(f'alpha= {self.alpha}, m= {self.m}')
+        plt.xticks(n_list)
+        plt.xlabel('Block size length', fontsize=self.fontsize)
+        plt.ylabel('Expected Number of Missmatches', fontsize=self.fontsize)
+        plt.title(f'alpha= {self.alpha}, m= {self.m}', fontsize=self.fontsize)
         plt.grid()
-        plt.legend()
+        plt.legend(fontsize=self.fontsize)
         
     def plot_exp_length(self, n_list, tau_list):
         '''Very simple plotting method'''
@@ -122,16 +124,28 @@ class KeyStat():
                y= self.exp_length(n, tau)
                ys.append(y)
              
-            plt.plot(n_list, ys, label=f'tau= {tau}')
+            plt.plot(n_list, ys, '--o', label=f'tau= {tau}')
         #plt.plot(n_list, probMismatch, label='key mismatch prob')
-        plt.xlabel('Block size length')
-        plt.ylabel('Expected length of key sequence')
-        plt.title(f'alpha= {self.alpha}, m= {self.m}')
+        plt.xticks(n_list)
+        plt.xlabel('Block size length', fontsize=self.fontsize)
+        plt.ylabel('Expected length of key sequence', fontsize=self.fontsize)
+        plt.title(f'alpha= {self.alpha}, m= {self.m}', fontsize=self.fontsize)
         plt.grid()
-        plt.legend()
-        
+        plt.legend(fontsize=self.fontsize)
+
 
     def plot_everything(self, n_list, tau_list):
         self.plot_exp_length(n_list, tau_list)
         self.plot_exp_no_missmathces(n_list, tau_list)
         self.plot_prob_matching_keys(n_list, tau_list)
+
+
+if __name__ == '__main__':
+    '''runs the whole thing, alpha and tau need to be in an array'''
+    alpha = 0.1
+    n_list = [5, 6, 7, 8, 9, 10]
+    m = 300
+    tau_list = [1, 2, 3]
+    stats = KeyStat(alpha, m)
+    stats.plot_everything(n_list, tau_list)
+    plt.show()
