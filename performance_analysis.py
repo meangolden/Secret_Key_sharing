d2bbb1fcf600a5a406afce5c0ad7b6e1d1d14d22
gugi200 <78741702+gugi200@users.noreply.github.com>
2022-08-19 16:54:11 +0100

    added excel, save figures

--- performance_analysis.py
+++ performance_analysis.py
@@ -7,6 +7,9 @@ Created on Thu Aug 18 21:16:46 2022
 import matplotlib.pyplot as plt
 from scipy.special import binom
 import matplotlib.font_manager as font_manager
+import pandas as pd
+import numpy as np
+
 
 class KeyStat():
 
@@ -15,6 +18,8 @@ class KeyStat():
         self.alpha=alpha
         self.m = m
         self.fontsize=11
+        self.NoMis = []
+        self.length = []
         self.font = font_manager.FontProperties(family='CMU Serif', size=11)
 
 
@@ -90,9 +95,11 @@ class KeyStat():
         plt.xticks(n_list)
         plt.xlabel('Block size length')
         plt.ylabel('Probability of two matching keys')
-        plt.title(r'$\alpha$= {}, m= {}'.format(self.alpha,self.m),fontsize=11)
+        #plt.title(r'$\alpha$= {}, m= {}'.format(self.alpha,self.m),fontsize=11)
         plt.grid()
         plt.legend(fontsize=10)
+        plt.savefig(f"plot/matchingKeys_alp{self.alpha}_m{self.m}.pdf", format="pdf", bbox_inches="tight")
+        plt.savefig(f"plot/matchingKeys_alp{self.alpha}_m{self.m}.eps", format="eps")
         
         
         
@@ -105,15 +112,18 @@ class KeyStat():
             for n in n_list:
                 y= self.exp_missmatches(n, tau)
                 ys.append(y)
+                self.NoMis.append(round(y, 4))
                  
             plt.plot(n_list, ys, '--o', label= r'$\tau$ = {}'.format(tau))
         #plt.plot(n_list, probMismatch, label='key mismatch prob')
         plt.xticks(n_list)
         plt.xlabel('Block size length')
         plt.ylabel('Expected Number of Missmatches')
-        plt.title(r'$\alpha$= {}, $m$= {}'.format(self.alpha,self.m),fontsize=11)
+        #plt.title(r'$\alpha$= {}, $m$= {}'.format(self.alpha,self.m),fontsize=11)
         plt.grid()
         plt.legend()
+        plt.savefig(f"plot/expNoMissmathces_alp{self.alpha}_m{self.m}.pdf", format="pdf", bbox_inches="tight")
+        plt.savefig(f"plot/expNoMissmathces_alp{self.alpha}_m{self.m}.eps", format="eps")
         
     def plot_exp_length(self, n_list, tau_list):
         '''Very simple plotting method'''
@@ -124,15 +134,18 @@ class KeyStat():
             for n in n_list:
                y= self.exp_length(n, tau)
                ys.append(y)
+               self.length.append(round(y, 4))
              
             plt.plot(n_list, ys, '--o', label= r'$\tau$ = {}'.format(tau))
         #plt.plot(n_list, probMismatch, label='key mismatch prob')
         plt.xticks(n_list)
         plt.xlabel('Block size length')
         plt.ylabel('Expected length of key sequence')
-        plt.title(r'$\alpha=$ {}, $m$= {}'.format(self.alpha,self.m),fontsize=11)
+        #plt.title(r'$\alpha=$ {}, $m$= {}'.format(self.alpha,self.m),fontsize=11)
         plt.grid()
         plt.legend()
+        plt.savefig(f"plot/expLength_alp{self.alpha}_m{self.m}.pdf", format="pdf", bbox_inches="tight")
+        plt.savefig(f"plot/expLength_alp{self.alpha}_m{self.m}.eps", format="eps")
 
 
     def plot_everything(self, n_list, tau_list):
@@ -140,6 +153,32 @@ class KeyStat():
         self.plot_exp_no_missmathces(n_list, tau_list)
         self.plot_prob_matching_keys(n_list, tau_list)
 
+
+
+    def createDataframe(self, n_list, tau_list):
+        tau_list = np.array(tau_list)
+        n_list = np.array(n_list)
+        length = np.array(self.length)
+        noMis = np.array(self.NoMis)
+        data = pd.DataFrame(columns=['m (length of)', 'alpha', 'n (block size)', 'tau (decision threshold)',
+        'No_mism (expected number of mismatches)', 'L  (expected length of final key)', 
+        'expected number of mismatches (%)'])
+        m_list = np.repeat(np.array([self.m]), len(tau_list)*len(n_list))
+        alpha_list = np.repeat(np.array([self.alpha]), len(tau_list)*len(n_list))
+        n_pd = np.tile(n_list, len(tau_list))
+        tau_list = np.array(tau_list)
+        tau_pd = np.repeat(tau_list, len(n_list))
+
+
+        data['m (length of)'] = m_list
+        data['alpha'] = alpha_list
+        data['n (block size)'] = n_pd
+        data['tau (decision threshold)'] = tau_pd
+        data['No_mism (expected number of mismatches)'] = noMis
+        data['L  (expected length of final key)'] = length
+        data['expected number of mismatches (%)'] = noMis/length*100
+
+        data.to_csv('dataKeyGenStat.csv')
 
 if __name__ == '__main__':
     '''runs the whole thing, alpha and tau need to be in an array'''
@@ -153,4 +192,5 @@ if __name__ == '__main__':
     tau_list = [1, 2, 3]
     stats = KeyStat(alpha, m)
     stats.plot_everything(n_list, tau_list)
+    stats.createDataframe(n_list, tau_list) # this function reuses parts of plot_everything
     plt.show()
